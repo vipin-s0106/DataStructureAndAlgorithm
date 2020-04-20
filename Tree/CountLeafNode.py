413e2c58ef994d9ed826a10ec4b8c53de33c1e87
Vipin <vipin.s0106@gmail.com>
2020-04-20 20:00:41 +0530

    code added in Tree, Linkedlist, Stack

---
+++ Tree/CountLeafNode.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class Tree:
+
+    def __init__(self):
+        self.root = None
+
+    def inorder_traversal(self,root):
+        if root == None:
+            return
+        self.inorder_traversal(root.left)
+        print(root.data,end=" ")
+        self.inorder_traversal(root.right)
+
+    def countLeafNode(self,node):
+        if node == None:
+            return 0
+        if node.left == None and node.right == None:
+            return 1
+        return self.countLeafNode(node.left) + self.countLeafNode(node.right)
+
+
+
+
+t1 = Tree()
+t1.root = Node(1)
+t1.root.left = Node(2)
+t1.root.right = Node(3)
+t1.root.left.left = Node(4)
+t1.root.left.right = Node(5)
+t1.root.right.left = Node(6)
+t1.root.right.right = Node(7)
+t1.root.right.right.left = Node(8)
+
+'''
+           1 
+         /   \ 
+        2      3 
+     /    \    /  \ 
+   4      5   6     7
+                   /
+                  8
+
+'''
+
+print(t1.countLeafNode(t1.root))
