73cc9f717cb3c1935af992f8dffea250fc1dcafb
Vipin <vipin.s0106@gmail.com>
2020-05-07 17:46:14 +0530

    IsBST method added

--- Tree/IsBST.py
+++ Tree/IsBST.py
@@ -29,22 +29,12 @@ class BST:
 			else:
 				prev.right = Node(data)
 
-	def checkIsBST(self,prev,cur):
-		if cur == None:
-			return
-		if cur.left != None or cur.right != None:
-			return self.checkIsBST(cur,cur.left) and self.checkIsBST(cur,cur.right)
-		else:
-			if prev.left == cur:
-				if cur.data < prev.data:
-					return True
-				else:
-					return False
-			else:
-				if cur.data > prev.data:
-					return True
-				else:
-					return False
+	def checkIsBST(self,node,upper,lower):
+		if node == None:
+                        return True
+                if node.data <= lower or node.data >= upper:
+                        return False
+                return check_tree(node.left,node.data,lower) and check_tree(node.right,upper,node.data)
 
 
 '''
@@ -63,4 +53,4 @@ t1.root.left.left = Node(5)
 t1.root.left.right = Node(15)
 t1.root.right.left = Node(25)
 t1.root.right.right = Node(35)
-print(t1.checkIsBST(None,t1.root))
+print(t1.checkIsBST(t1.root,100000,-100000))
